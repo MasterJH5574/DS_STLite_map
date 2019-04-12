ba15d3fe5813a097d1de245d6a19313708899220
MasterJH5574 <45167100+MasterJH5574@users.noreply.github.com>
2019-04-12 11:32:57 +0800

    final version with accept

--- map.hpp
+++ map.hpp
@@ -16,20 +16,44 @@ template<
 public:
     typedef pair<const Key, T> value_type;
 
-private:
+public:
     class RBT {
     public:
         struct node {
-            value_type value;
+            value_type *value;
             bool color; // 0 -> black, 1 -> red
             node *son[2], *fa;
             node *last, *next;
 
-            node() {}
-            node(const value_type &val, node* _nil = nullptr) : value(val) {
+            node() : value(nullptr), color(0) {
+                son[0] = son[1] = fa = nullptr;
+                last = next = nullptr;
+            }
+            node(const value_type &val, node* _nil = nullptr) : value(new value_type(val)) {
                 color = 0;
                 last = next = _nil;
                 son[0] = son[1] = fa = _nil;
+            }
+            node(const node &o) {
+                value = new value_type(*o.value);
+                color = o.color;
+                last = o.last, next = o.next;
+                son[0] = o.son[0], son[1] = o.son[1];
+                fa = o.fa;
+            }
+            ~node() {
+                if (value != nullptr)
+                    delete value;
+            }
+            node &operator=(const node &o) {
+                if (this == &o)
+                    return *this;
+                delete value;
+                value = new value_type(*o.value), color = o.color;
+                son[0] = o.son[0], son[1] = o.son[1];
+                fa = o.fa;
+                last = o.last, next = o.next;
+                return *this;
             }
         } *nil, *root;
 
@@ -41,10 +65,9 @@ private:
                 o = nil;
                 return;
             }
-            o = new node;
+            o = new node(*oo->value, nil);
             o->fa = f;
             o->color = oo->color;
-            o->value = oo->value;
 
             construct(o->son[0], oo->son[0], o, oo_nil, last_create);
 
@@ -56,20 +79,21 @@ private:
         }
     public:
         RBT() {
-            nil = new node();
+            nil = new node;
             nil->son[0] = nil->son[1] = nil->fa = nil;
 
             root = nil;
             _size = 0;
         }
         RBT(const RBT &o) {
-            nil = new node();
+            nil = new node;
             nil->son[0] = nil->son[1] = nil->fa = nil;
 
             node *last_create = nil;
             construct(root, o.root, nil, o.nil, last_create);
             last_create->next = nil;
             nil->last = last_create;
+            _size = o._size;
         }
         ~RBT() {
             if (root != nil)
@@ -87,6 +111,8 @@ private:
             construct(root, o.root, nil, o.nil, last_create);
             last_create->next = nil;
             nil->last = last_create;
+            _size = o._size;
+            return *this;
         }
 
         void clear(const node *p) {
@@ -97,23 +123,19 @@ private:
             delete p;
         }
 
-        size_t size() {
-            return _size;
-        }
-
-        node* find(const Key &k) {
+        node* find(const Key &k) const {
             node *p = root;
-            while (p != nil && p->value.first != k)
-                p = cmp(k, p->value.first) ? p->son[0] : p->son[1];
+            while (p != nil && (cmp(p->value->first, k) || cmp(k, p->value->first)))
+                p = cmp(k, p->value->first) ? p->son[0] : p->son[1];
             return p == nil ? nil : p;
         }
 
-        node* find_insert(const Key &k) {
+        node* find_insert(const Key &k) const {
             node *p = root;
             while (1) {
-                if (!cmp(k, p->value.first) && !cmp(p->value.first, k)) // insert fail
+                if (!cmp(k, p->value->first) && !cmp(p->value->first, k)) // insert fail
                     return nil;
-                if (cmp(k, p->value.first)) {
+                if (cmp(k, p->value->first)) {
                     if (p->son[0] == nil)
                         return p;
                     p = p->son[0];
@@ -133,8 +155,18 @@ private:
                 p = p->son[0];
             return p;
         }
-
         node *end() {
+            return nil;
+        }
+        const node* cbegin() const {
+            if (_size == 0)
+                return nil;
+            const node *p = root;
+            while (p->son[0] != nil)
+                p = p->son[0];
+            return p;
+        }
+        const node *cend() const {
             return nil;
         }
 
@@ -143,7 +175,9 @@ private:
             int l = f->son[1] == o, r = l ^ 1;
 
             f == root ? root = o : ff->son[ff->son[1] == f] = o;
-            o->fa = ff, f->fa = o, o->son[r]->fa = f;
+            o->fa = ff, f->fa = o;
+            if (o->son[r] != nil)
+                o->son[r]->fa = f;
             f->son[l] = o->son[r], o->son[r] = f;
         }
 
@@ -170,9 +204,9 @@ private:
             }
             root->color = 0;
         }
-
         node* insert(const value_type &val) {
             if (root == nil) {
+                _size++;
                 root = new node(val, nil);
                 return root;
             }
@@ -181,9 +215,10 @@ private:
             if (p == nil) // insert fail
                 return nil;
 
+            _size++;
             node *q = new node(val, nil);
             q->fa = p;
-            if (cmp(val.first, p->value.first)) {
+            if (cmp(val.first, p->value->first)) {
                 p->son[0] = q;
                 q->last = p->last;
                 p->last->next = q;
@@ -192,7 +227,7 @@ private:
             } else {
                 p->son[1] = q;
                 q->next = p->next;
-                p->next.last = q;
+                p->next->last = q;
                 p->next = q;
                 q->last = p;
             }
@@ -201,28 +236,31 @@ private:
             insert_maintain(q);
             nil->color = 0;
             nil->son[0] = nil->son[1] = nil->fa = nil->next = nil->last = nil;
+            return q;
         }
 
         void swap_info(node *x, node *y) {
-            node *tmp1 = new node(x), *tmp2 = new node(y);
+            node *tmp1 = new node, *tmp2 = new node;
 
+            *tmp1 = *x;
             x->son[0]->fa = tmp1, x->son[1]->fa = tmp1;
             x->fa->son[x->fa->son[1] == x] = tmp1;
             x->last->next = tmp1, x->next->last = tmp1;
 
+            *tmp2 = *y;
             y->son[0]->fa = tmp2, y->son[1]->fa = tmp2;
             y->fa->son[y->fa->son[1] == y] = tmp2;
             y->last->next = tmp2, y->next->last = tmp2;
 
-            &y = &tmp1, &x = &tmp2;
-
+            *y = *tmp1;
             tmp1->son[0]->fa = y, tmp1->son[1]->fa = y;
             tmp1->fa->son[tmp1->fa->son[1] == tmp1] = y;
             tmp1->last->next = y, tmp1->next->last = y;
 
+            *x = *tmp2;
             tmp2->son[0]->fa = x, tmp2->son[1]->fa = x;
             tmp2->fa->son[tmp2->fa->son[1] == tmp2] = x;
-            tmp2->last->next = x, tmp2->next->next = x;
+            tmp2->last->next = x, tmp2->next->last = x;
 
             delete tmp1;
             delete tmp2;
@@ -257,13 +295,18 @@ private:
             }
             x->color = 0;
         }
-
         void erase(node *z) {
+            _size--;
             node *y = z->son[0] == nil || z->son[1] == nil ? z : z->next;
             if (y != z) {
+                if (z == root)
+                    root = y;
                 swap_info(y, z);
                 node *t = y;
                 y = z, z = t;
+                value_type *tmp = y->value;
+                delete z->value;
+                z->value = new value_type(*y->value);
             }
 
             node *x = y->son[0] != nil ? y->son[0] : y->son[1];
@@ -293,20 +336,23 @@ private:
 	 */
 
 public:
+    class const_iterator;
 	class iterator {
-	public:
+	    friend void map::erase(iterator pos);
+	    friend const_iterator;
+
+	    typedef pair<const Key, T> value_type;
+	    typedef value_type&         reference;
+	    typedef value_type*           pointer;
+
+    private:
 	    map *_map;
 	    typename RBT::node *p;
 
 	public:
 		iterator() : _map(nullptr), p(nullptr) {}
-		iterator(const iterator &o) : _map(nullptr), p(o.p) {}
-		iterator(const map *&__map, const RBT *&_rbt, const typename RBT::node *&_p) : _map(__map), p(_p) {}
-		/**
-		 * return a new iterator which pointer n-next elements
-		 *   even if there are not enough elements, just return the answer.
-		 * as well as operator-
-		 */
+		iterator(const iterator &o) : _map(o._map), p(o.p) {}
+		iterator(map *__map, typename RBT::node *_p) : _map(__map), p(_p) {}
 
 		iterator operator++(int) {
             if (*this == _map->end())
@@ -345,54 +391,138 @@ public:
             return *this;
 		}
 
-		value_type &operator*() const {
-		    return p->value;
+		reference operator*() const {
+		    return *p->value;
 		}
-		value_type* operator->() const noexcept {
-		    return &(p->value);
+		pointer operator->() const noexcept {
+		    return &(*p->value);
 		}
 
         bool operator==(const iterator &o) const {
             return _map == o._map && p == o.p;
         }
+        bool operator==(const const_iterator &o) const {
+            return _map == o._map && p == o.p;
+        }
         bool operator!=(const iterator &o) const {
             return _map != o._map || p != o.p;
         }
+        bool operator!=(const const_iterator &o) const {
+            return _map != o._map || p != o.p;
+        }
 	};
-	using const_iterator = iterator;
+    class const_iterator {
+        friend iterator;
 
+        typedef const pair<const Key, T> value_type;
+        typedef value_type&         reference;
+        typedef value_type*           pointer;
+
+    private:
+        const map *_map;
+        const typename RBT::node *p;
+
+    public:
+        const_iterator() : _map(nullptr), p(nullptr) {}
+        const_iterator(const iterator &o) : _map(o._map), p(o.p) {}
+        const_iterator(const const_iterator &o) : _map(o._map), p(o.p) {}
+        const_iterator(const map *__map, const typename RBT::node *_p) : _map(__map), p(_p) {}
+
+        const_iterator operator++(int) {
+            if (*this == _map->cend())
+                throw invalid_iterator();
+            const const_iterator res(*this);
+            p = p->next;
+            return res;
+        }
+        const_iterator &operator++() {
+            if (*this == _map->cend())
+                throw invalid_iterator();
+            p = p->next;
+            return *this;
+        }
+        const_iterator operator--(int) {
+            if (*this == _map->cbegin())
+                throw invalid_iterator();
+            const const_iterator res(*this);
+            if (p == _map->tr.nil) {
+                p = _map->tr.root;
+                while (p->son[1] != _map->tr.nil)
+                    p = p->son[1];
+            } else
+                p = p->last;
+            return res;
+        }
+        const_iterator &operator--() {
+            if (*this == _map->cbegin())
+                throw invalid_iterator();
+            if (p == _map->tr.nil) {
+                p = _map->tr.root;
+                while (p->son[1] != _map->tr.nil)
+                    p = p->son[1];
+            } else
+                p = p->last;
+            return *this;
+        }
+
+        reference operator*() const {
+            return *p->value;
+        }
+        pointer operator->() const noexcept {
+            return &(*p->value);
+        }
+
+        bool operator==(const iterator &o) const {
+            return _map == o._map && p == o.p;
+        }
+        bool operator==(const const_iterator &o) const {
+            return _map == o._map && p == o.p;
+        }
+        bool operator!=(const iterator &o) const {
+            return _map != o._map || p != o.p;
+        }
+        bool operator!=(const const_iterator &o) const {
+            return _map != o._map || p != o.p;
+        }
+    };
+
+
+public:
     map() {}
-    map(const map &other) = default;
+    map(const map &o) = default;
     ~map() = default;
     map &operator=(const map &o) = default;
 
     void clear() {
-        tr.clear(tr.root);
+        if (tr.root != tr.nil)
+            tr.clear(tr.root);
+        tr._size = 0;
+        tr.root = tr.nil;
     }
 
-	T& at(const Key &k) {
+	T &at(const Key &k) {
         typename RBT::node *p = tr.find(k);
 	    if (p != tr.nil)
-	        return p;
+	        return p->value->second;
 	    throw index_out_of_bound();
 	}
-	const T & at(const Key &k) const {
+	const T& at(const Key &k) const {
         const typename RBT::node *p = tr.find(k);
         if (p != tr.nil)
-            return p;
+            return p->value->second;
         throw index_out_of_bound();
 	}
 
-	T & operator[](const Key &k) {
+	T &operator[](const Key &k) {
 	    typename RBT::node *p = tr.find(k);
 	    if (p == tr.nil)
-	        return tr.insert(value_type(k, T()))->value.second;
-	    return p->value.second;
+	        return tr.insert(value_type(k, T()))->value->second;
+	    return p->value->second;
 	}
-	const T & operator[](const Key &k) const {
+	const T& operator[](const Key &k) const {
         const typename RBT::node *p = tr.find(k);
         if (p != tr.nil)
-            return p;
+            return p->value->second;
         throw index_out_of_bound();
 	}
 
@@ -401,7 +531,7 @@ public:
 	    return iterator(this, p);
 	}
 	const_iterator cbegin() const {
-        typename RBT::node *p = tr.begin();
+        const typename RBT::node *p = tr.cbegin();
         return const_iterator(this, p);
 	}
 	iterator end() {
@@ -409,15 +539,15 @@ public:
 	    return iterator(this, p);
 	}
 	const_iterator cend() const {
-        typename RBT::node *p = tr.end();
+        const typename RBT::node *p = tr.cend();
         return const_iterator(this, p);
 	}
 
 	bool empty() const {
-	    return tr.size() == 0;
+	    return tr._size == 0;
 	}
 	size_t size() const {
-	    return tr.size();
+	    return tr._size;
 	}
 
 	/**
@@ -429,13 +559,12 @@ public:
 	pair<iterator, bool> insert(const value_type &value) {
 	    typename RBT::node *p = tr.insert(value);
 	    if (p == tr.nil)
-	        return pair<iterator, bool>(iterator(this, tr.find(value.first)), false);
+            return pair<iterator, bool>(iterator(this, tr.find(value.first)), false);
 	    return pair<iterator, bool>(iterator(this, p), true);
 	}
 
 	/**
 	 * erase the element at pos.
-	 *
 	 * throw if pos pointed to a bad element (pos == this->end() || pos points an element out of this)
 	 */
 	void erase(iterator pos) {
@@ -445,22 +574,16 @@ public:
 	}
 
 	size_t count(const Key &key) const {
-	    return tr.find(key) != tr.nil;
+	    return tr.find(key) != tr.nil ? 1 : 0;
 	}
 
-	/**
-	 * Finds an element with key equivalent to key.
-	 * key value of the element to search for.
-	 * Iterator to an element with key equivalent to key.
-	 *   If no such element is found, past-the-end (see end()) iterator is returned.
-	 */
 	iterator find(const Key &key) {
 	    typename RBT::node *p = tr.find(key);
 	    return p == tr.nil ? end() : iterator(this, p);
 	}
 	const_iterator find(const Key &key) const {
 	    const typename RBT::node *p = tr.find(key);
-        return p == tr.nil ? end() : const_iterator(this, p);
+        return p == tr.nil ? cend() : const_iterator(this, p);
 	}
 };
 
